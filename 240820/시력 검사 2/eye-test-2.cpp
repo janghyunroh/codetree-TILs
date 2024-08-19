86cc856987c0c626dd13110733edee582b658a79
Janghyun Roh <113655323+janghyunroh@users.noreply.github.com>
2024-08-19 23:13:31 +0800

    Title: 시력 검사 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240820/시력 검사 2/eye-test-2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float avg; cin >> avg;
+    if(avg >= 1.0) cout << "High";
+    else if(avg >= 0.5) cout << "Middle";
+    else cout << "Low";
+    return 0;
+}
