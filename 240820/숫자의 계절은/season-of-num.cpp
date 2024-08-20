1cb71cc511fb9aa1b5c5afd2340e3eb771a2f548
Janghyun Roh <113655323+janghyunroh@users.noreply.github.com>
2024-08-20 14:02:13 +0800

    Title: 숫자의 계절은, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240820/숫자의 계절은/season-of-num.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int m; cin >> m;
+    if(m==12||m<3) cout << "Winter";
+    else if(m < 6) cout << "Spring";
+    else if(m < 9) cout << "Summer";
+    else cout << "Fall";
+    return 0;
+}
