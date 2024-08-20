c9f739e21cc0440d7ab11efe6b2127b146f9edc1
Janghyun Roh <113655323+janghyunroh@users.noreply.github.com>
2024-08-20 14:24:38 +0800

    Title: 일 수 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240820/일 수 구하기/number-of-days-in-month.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n; cin >> n;
+    if(n==2) cout << 28;
+    else if(n < 8) {
+        if(n % 2 == 0) cout << 30;
+        else cout << 31;
+    }
+    else {
+        if(n%2==0) cout << 31;
+        else cout << 30;
+    }
+    return 0;
+}
