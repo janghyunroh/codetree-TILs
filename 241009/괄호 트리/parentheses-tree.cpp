ce1b75ed52d2d036416be1bfee4a8e2078392eb6
Janghyun Roh <113655323+janghyunroh@users.noreply.github.com>
2024-10-09 19:40:35 +0900

    Title: 괄호 트리, Time: 334ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 241009/괄호 트리/parentheses-tree.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+
+// 스택 필요 없을 듯?
+
+string s;
+int main() {
+    cin >> s;
+    int cur_depth = 0;
+    int ans = 0;
+    char prev_par;
+    for(int i =0; i < s.length(); i++) {
+        if(s[i] == '(') {
+            cur_depth += 1;
+        }
+        else {
+            cur_depth -= 1;
+            if(prev_par == '(') ans += cur_depth;
+        }
+        prev_par = s[i];
+    }
+    cout << ans;
+    return 0;
+}
