87347d25febb9bef01e115ff2b4b057c0e6a45ea
Janghyun Roh <113655323+janghyunroh@users.noreply.github.com>
2024-10-09 19:30:47 +0900

    Title: 집 찾아가기, Time: 27ms, Memory: 14MB, Status: Passed - CodeTree

--- 241009/집 찾아가기/find-the-house.cpp
+++ 241009/집 찾아가기/find-the-house.cpp
@@ -13,16 +13,16 @@ int main() {
     for(int i = 0; i < n; i++) {
         int x; char c; int dist; 
         cin >> x >> c >> dist;
-        arr[x] = make_pair(c, dist);
+        arr[x + 1000000] = make_pair(c, dist);
     }
     cin >> cur_pos;
 
     //simulate
     for(int i = 0; i < n; i++) {
-        if(get<0>(arr[cur_pos]) == 'R')
-            cur_pos += get<1>(arr[cur_pos]);
+        if(get<0>(arr[cur_pos + 1000000]) == 'R')
+            cur_pos += get<1>(arr[cur_pos + 1000000]);
         else {
-            cur_pos -= get<1>(arr[cur_pos]);
+            cur_pos -= get<1>(arr[cur_pos + 1000000]);
         }
     }
     cout << cur_pos;
